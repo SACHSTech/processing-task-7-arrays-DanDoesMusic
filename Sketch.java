785eb41742bc1cfbd41ab5e16a4ce2f6086503b1
DanDoesMusic <78486648+DanDoesMusic@users.noreply.github.com>
2022-01-12 17:54:47 +0000

    snow

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,56 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+ int num = 50;
+ int []  Rectangle = new int [num];
+int[] x = new int[num];
+int[] y = new int[num];
+float[] circleY = new float[25];
+  public void settings(){
+    size (400,400);
+  }
 	
 	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
+public void setup() {
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  noStroke();
+  fill(255, 102);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  for (int i = 0; i < circleY.length; i++) {
+    circleY[i] = random(height);
+}
+}
+
+public void draw() {
+  background(0);
+  // Shift the values to the right
+  for (int i = num-1; i > 0; i--) {
+    x[i] = x[i-1];
+    y[i] = y[i-1];
   }
-  
-  // define other methods down here.
+  // Add the new values to the beginning of the array
+  x[0] = mouseX;
+  y[0] = mouseY;
+  // Draw the circles
+  for (int i = 0; i < num; i++) {
+    ellipse(x[i], y[i], 30 - i , 30 - i);
+  }
+    for (int i = 0; i < circleY.length; i++) {
+    float circleX = width * i / circleY.length;
+    fill(255,0,0);
+    ellipse(circleX, circleY[i], 5, 5);
+
+    circleY[i] ++;
+
+
+    if (circleY[i] > height) {
+      circleY[i] = 0;
+          Rectangle[i]++;
+    }
+  }
+for (int i = 0; i < 20 ; i++ ){
+  rect(i * 20, 400, 25, Rectangle[i] * -2 );
+}
+}
 }
